7142cbf9eb2a75e899031893fced62e1a4048628
hr645364 <128062621+hr645364@users.noreply.github.com>
2023-05-16 10:58:54 -0700

    complete assingment

---
+++ app.js
@@ -0,0 +1,572 @@
+                         //chater1satrt//
+                        
+
+// alert("First Name");
+// alert("Last Name");
+// alert("Email");
+// alert("Phone Number");
+// alert("Password");
+
+// alert("You're learning JavaScript!")
+
+//    alert("chapter one complet");
+
+
+                        // chapter1 end//
+
+
+// .......................................................////
+
+                        //chapter2satrt//
+
+// var chapter2 = "string var";
+// console.log(chapter2);
+
+
+// var team = "dusrin tream";
+// alert("hamari team");
+
+// console.log(team);
+
+
+// var batsman = "Charlie";
+
+// batsman = "john"
+// console.log(batsman);
+                        
+                        //chapter2end//
+
+//................................................//
+
+
+                        // chapter3satrt//
+
+                        
+// var chapter3 =  "caseQty" ;
+//  console.log(chapter3);
+
+
+
+//  var num  = 144;
+
+//  num = 45;
+
+//  console.log(num);
+
+
+
+//  var number = 9
+
+//  console.log( 5 +number);
+
+
+
+// var result = //variable;
+
+// result = 5 + 10;
+//  console.log(result);
+
+
+
+
+
+// var fristamount = 100;
+// var  secamount = 100;
+
+// var result = fristamount + secamount;
+// console.log(result);
+
+
+
+
+
+
+
+// var myNumber; 
+
+// myNumber = 5; 
+
+// myNumber = myNumber + 3; 
+// console.log(myNumber);
+
+
+
+      
+                           // chapter3end//
+//............................................................//
+
+                      // chapter4satrt//
+// var productCost = 3.45;
+
+// var  finalcost = 54;
+
+// console.log(finalcost);
+
+//  var Nameofband = " hamid raza" ;
+//  Nameofband =  'raza';
+//  console.log(Nameofband)
+
+/// baki he abhi //
+
+
+                       //chapter4 end//
+ //...........................................................................//
+                   // chapter5satrt//
+
+// var remin = 100 % 100;
+// console.log(remin);
+
+// var num = 20 % 5;
+//  console.log(num);
+
+
+// var First = "";
+// result = 60* 80;
+// console.log(result);
+
+
+
+
+// var sec = "";
+// res = 60 - 50;
+// console.log(res);
+
+// const remainder = 10 % 3;
+
+// console.log(remainder);
+
+//   var mul = ""
+//   mul = 10 * 50;
+//   console.log(mul);
+                     // chapter5end//
+       //..........................................................//
+
+
+                //chapter6satrt//
+
+
+//  var x = 5;
+// x++; 
+// console.log(x); 
+
+// var y = 10;
+// ++y; 
+// console.log(y);
+
+// var x = 100;
+//  x =  x- 1;
+// console.log(x); 
+
+
+// var x = 50;
+// var y = x++;
+
+// console.log(x);
+
+// var y = 50;
+// var z = --y;
+// console.log(z);
+
+// var num = 10;
+// var  newNum = num--;
+// console.log(num);    
+// console.log(newNum);
+
+
+// let num1 = 5;
+// let num2 = num1++;
+// console.log(num1);
+// console.log(num2);  
+
+
+// var  num = 5; 
+
+// num++; 
+
+// alert(num); 
+             //chapter6end//
+ //......................................................//
+
+
+                   //  chapter7start///
+
+//    var calculatedNum = 2 + (2 * 6);
+//    console.log(calculatedNum);
+
+//    var calculatedNum = (2 + 2) * 6;
+//    console.log(calculatedNum);
+
+// var calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum);
+
+
+// var calculatedNum = ((2 + 2) * 4) + 2;
+//  console.log(calculatedNum);
+
+// var calculatedNum = ((2 + 2) * (4 + 10))
+// console.log(calculatedNum);
+
+// var calculatedNum = (2 + 2 * 4 + 10)
+//  console.log(calculatedNum);
+
+//  var calculatedNum = ( (4 / 2 )* 4)
+//  console.log(calculatedNum);
+         
+
+           //  chapter7end///
+
+      //..........................................................//     
+
+
+    //   chapter8start//
+
+    // var num = "2" + "2";
+    // console.log(num);
+
+//    var  message = ("Hello," + "Dolly");
+
+//    alert(message);
+
+
+// var num = ("33" + "3")
+// alert(num);
+
+
+// var part1 = "Pakistan";
+// var part2 = "Zindabad";
+// var message = part1 + " " + part2;
+
+// alert(message);
+
+// var myVariable = "The number is: " + 42;
+
+// console.log(myVariable);
+
+
+// var firststring = "firststring"
+
+// var secstring = "secstring"
+// var result = firststring +  secstring;
+// console.log(result)
+
+
+        //   chapter8end///
+
+
+        //...................................................//
+
+
+        //chapter9start//
+// var firstname = prompt("enter your fist name");
+// console.log(firstname);
+
+
+// var country = prompt("Country?", "China");
+// console.log(country);
+
+// var yourName = prompt("Enter Your Name");
+// console.log(yourName);
+
+
+
+// var userInput = prompt("Please enter your favorite color:", "Blue");
+// console.log(userInput); 
+
+// var message = "Enter your name:";
+// var defaultResponse = "John Doe";
+
+// var userResponse = prompt(message, defaultResponse);
+
+// console.log(userResponse);
+
+//  var message = "Please enter your age:";
+//  var defaultResponse = "25";
+
+//  var userResponse = prompt(message, defaultResponse);
+
+
+// alert(userResponse);
+
+
+        // chapter9end//
+
+
+    //...........................................................//
+    
+    // chapter10 start//
+
+
+//     var city = "Karachi"
+//     if (city = "Karachi") {
+//     console.log("The City OF Lights")
+// }
+
+// if (x === y) {
+
+//     var z = prompt("Please enter the value of z:");
+//     z = 10;
+//     console.log(z);
+//   }
+// var ZipCode = "10010";
+
+// if (ZipCode === "10010") {
+//   alert("Karachi");
+// } else {
+//   alert("Please write correct city");
+// }
+
+
+// if (a === 10) {
+//     alert("a is 10");
+//   } else {
+//     alert("The correct value of a is");
+//   }
+  
+
+
+
+
+// var number = 7; 
+
+// if (number !== 5) {
+//   alert("Congratulations!"); 
+// }
+
+// var city = prompt("Enter a city:");
+
+// if (city === "Karachi") {
+//   alert("Acknowledging it is Karachi");}
+
+
+
+
+    //chapter 10end//
+//..........................................................//
+    //chapter11start//
+
+    // var number = 7; 
+
+    // if (number !== 5) {
+    //   alert("Congratulations!"); }
+
+
+
+    // var firstName = prompt("Enter your first name:");
+
+    // if (firstName !== "John") {
+    //   alert("No match");
+    // }
+    
+
+
+    
+// var num =  prompt("Enter Your City Name", "Karachi")
+//  if (city === "Karachi") {
+//    console.log("The City of Lights");
+//  }else{
+//      console.log("Not found")
+//  }
+
+
+// var marks = prompt("Enter your marks: ");
+
+
+// marks = parseFloat(marks);
+
+
+// var percentage = (marks / 100) * 100;
+
+
+// var grade;
+
+// if (percentage >= 90) {
+//   grade = "A";
+// } else if (percentage >= 80) {
+//   grade = "B";
+// } else if (percentage >= 70) {
+//   grade = "C";
+// } else if (percentage >= 60) {
+//   grade = "D";
+// } else {
+//   grade = "F";
+// }
+
+
+// console.log("Percentage: " + percentage.toFixed(2) + "%");
+// console.log("Grade: " + grade);
+
+ //chapter11end//
+
+ //..............................................//
+ //chapter12 start//
+
+ 
+//  var firstName = prompt("Enter your first name:");
+
+//  if (firstName !== "John") {
+//    alert("No match");
+//  }
+   
+ 
+//  if (a === 10) {
+//     alert("a is 10");
+//   } else {
+//     alert("The correct value of a is");
+//   }
+
+
+// var city = prompt("Enter a city:");
+
+// if (city === "Karachi") {
+//   alert("Acknowledging it is Karachi");
+// } else if (city === "Lahore") {
+//   alert("Acknowledging it is Lahore");
+// } else {
+//   alert("Different city entered");
+// }
+
+
+ //chapter12end//
+
+
+
+ //.................................................................//
+
+
+
+
+
+// chapter 14start//
+
+
+// var password = prompt("Enter your password:");
+
+// if (password !== "") {
+//   if (password.length <= 5) {
+//     alert("Password must be greater than 5");
+//   } else {
+//     alert("OK");
+//   }
+// }
+// var a ="hello"
+// if (a === 1) {
+
+    
+//     var c = " world"
+//     if (c === "Max") {
+//       alert("OK");
+//     }
+//   }
+
+// var variable1 = 5;
+// var variable2 = 5;
+
+// if (variable1 === variable2) {
+//   if (variable1 <= variable2) {
+//     alert("Test passed!");
+//   }
+// }
+
+
+
+//chapter14end//
+
+//......................................//
+
+
+// chapter15start//
+
+// var a = []
+
+// var b = ["hamid"]
+
+
+// var arr = ["h","i","j","k"]
+// console.log (arr[2]);
+
+
+
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+// var lengthOfArray = alphabet.length;
+
+// console.log(lengthOfArray); 
+
+//  chapter15end//
+
+//......................................................//
+
+// chapter16start//
+
+// var myArray = ["Hello"]; 
+// myArray.push("World"); 
+
+// var lastElement = myArray[myArray.length - 1]; 
+
+// alert(lastElement); 
+
+// var Alphabet = ["h", "i", "j", "k"];
+// Alphabet.pop();
+
+// console.log(Alphabet); 
+
+
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.shift();
+
+
+// var myArray = ["first element"];  // Declare an array with one element
+
+// myArray.unshift("second element"); // Add a second element to the beginning of the array
+
+// alert(myArray[0]); // Display an alert with the new first element
+
+
+
+
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+
+
+// var dogIndex = pets.indexOf("dog");
+
+// console.log(dogIndex);
+// chapter16end//
+
+
+
+// chapter17-20start//
+
+
+// for (var i = 0; i < 10; i++) {
+//     // Code to be executed within the loop
+//     console.log("Loop iteration:", i + 1);
+//   }
+  
+
+
+
+// for (var i = 0; i <= 12; i++) {
+//     // Code to be executed within the loop
+//     console.log("Loop iteration:", i);
+//   }
+  
+// for (var i = 0; i <= 4; i++) {
+    
+//   }
+  
+// for (var counter = 0; counter < 100; counter++) {
+//     // Code to be executed within the loop
+//     console.log("Loop iteration:", counter);
+//   }
+  
+
+// var firstArr = ["a", "b", "c", "d", "e", "f"];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+
+// for (var i = 0; i < firstArr.length; i++) {
+//   for (var j = 0; j < secondArr.length; j++) {
+//     console.log(firstArr[i] + secondArr[j]);
+//   }
+// }
+
+// chapter17-20end//
